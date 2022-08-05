eccd483860bf41f09af16bdabcc0fd0cf2b621cc
theash2 <2375229554@qq.com>
2022-08-05 16:58:54 +0800

    Revert "管理多个入口;源代码编译代码映射;开发模式:watch,简答服务器"
    This reverts commit 6fefc772bec2afef9489fff3ff33d12e54b682ff.

--- src/print.js
+++
@@ -1,3 +0,0 @@
-export default function PrintMe(){
-    console.log('需要输出',new Date());
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 module.exports = {
-    entry: {
-        'app': './src/index.js',
-        'print': './src/print.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000
-    },
-    mode: "development",
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: "output manage"
-        })
-    ],
+    entry: './src/index.js',
     output: {
-        filename: '[name].bundle.js',//输出编译完的文件的名字
+        filename: 'bundle.js',//输出编译完的文件的名字
         path: path.resolve(__dirname, 'dist')
     },
-    optimization: {
-        runtimeChunk: 'single', //多个入口时
-    },
-};
+  };
