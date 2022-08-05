0a41c211166fe299c2d902560fc9ced29d14bc75
theash2 <2375229554@qq.com>
2022-08-05 17:01:47 +0800

    Revert "Revert "管理多个入口;源代码编译代码映射;开发模式:watch,简答服务器""
    This reverts commit eccd483860bf41f09af16bdabcc0fd0cf2b621cc.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
-
+import PrintMe from "./print";
 function component() {
     let element = document.createElement('div');
 
     //lodash 引入通过import
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    let btn = document.createElement('button');
+    btn.innerHTML = '点击打印事件';
+    btn.onclick = PrintMe;
+    element.append(btn);
     return element;
 }
 
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function PrintMe(){
+    console.log('需要输出',new Date());
+}
